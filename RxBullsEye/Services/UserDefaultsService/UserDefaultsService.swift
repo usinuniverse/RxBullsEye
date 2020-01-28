9c773e66df7ea29f0a80176d555cb9dd733df66c
usinuniverse <usinuniverse@gmail.com>
2020-01-28 12:05:44 +0900

    feat: Implement UserDefaultsService

---
+++ RxBullsEye/Services/UserDefaultsService/UserDefaultsService.swift
@@ -0,0 +1,39 @@
+//
+//  UserDefaults.swift
+//  RxBullsEye
+//
+//  Created by jae hyeong ahn on 2020/01/28.
+//  Copyright © 2020 usinuniverse. All rights reserved.
+//
+
+import Foundation
+
+class UserDefaultsService: UserDefaultsServiceType {
+    // MARK: - Enum
+    
+    enum Key: String {
+        case hallOfFame
+    }
+    
+    // MARK: - Properties
+    
+    var serviceProvider: ServiceProviderType
+    private var userDefaults = UserDefaults.standard
+    
+    // MARK: - Initialization
+    
+    init(serviceProvider: ServiceProviderType) {
+        self.serviceProvider = serviceProvider
+    }
+    
+    // MARK: - Methods
+    
+    func set<T>(value: T, forKey key: String) {
+        self.userDefaults.set(value, forKey: key)
+    }
+    
+    func value(forKey key: String) -> Any? {
+        return self.userDefaults.value(forKey: key)
+    }
+    
+}
